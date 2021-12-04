d2e26d29fa9da468313535fdcef3185051195c95
PapadakisGeorge <george.papadakis@camelotls.com>
2021-12-04 19:05:20 +0200

    Dockerfile and action.yaml finalised

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
 const {getTestNames} = require('./src/sauceLabs/getTestNames');
-const {forEach} = require('p-iteration');
-
 const {getTestData} = require('./src/sauceLabs/getTestData');
 const {filterTests} = require('./src/utils/filter');
 const {testPassRate, testAverageDuration} = require('./src/utils/data_analysis');
+const {forEach} = require('p-iteration');
 const Report = require('./src/reporter/reporter');
 
-
 const start = async () => {
-    const builds = [
-        'Execute_IOS_Tests',
-        'Execute_Android_Tests',
-    ];
 
     const report = {};
-    await forEach(builds, async (build) => {
-        const testNames = await getTestNames(build);
-        await forEach(testNames, async (testName) => {
-            const testData = await getTestData(testName);
-            let filteredTestData = filterTests(testData, 'build', build);
-            filteredTestData = filterTests(filteredTestData, 'build', 'master');
+    const testNames = await getTestNames(process.env.BRANCH_NAME);
+    await forEach(testNames, async (testName) => {
 
+        const testData = await getTestData(testName);
+        let filteredTestData = filterTests(testData, 'build', process.env.BRANCH_NAME);
 
-            if (testPassRate(filteredTestData) && testPassRate(filteredTestData) < 90) {
-                report[`${testName}`] = new Report(
-                    `${testAverageDuration(filteredTestData) + '%'}`,
-                    `${testPassRate(filteredTestData)}`,
-                );
-            }
-        })
+        if (testPassRate(filteredTestData) && testPassRate(filteredTestData) < 90) {
+            report[`${testName}`] = new Report(
+                `${testAverageDuration(filteredTestData) + '%'}`,
+                `${testPassRate(filteredTestData)}`,
+            );
+        }
+    })
 
-
-    });
     console.table(report)
 }
 
